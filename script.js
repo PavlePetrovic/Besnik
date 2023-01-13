13110767b5b8f239fa6cf751f1927dd1a5b43e5d
PavlePetrovic <94621983+PavlePetrovic@users.noreply.github.com>
2023-01-13 18:55:59 +0100

    Cleanup code 1 (#11)
    * Code cleanup
    
    * Js code cleanup

--- script.js
+++ script.js
@@ -1,21 +1,23 @@
+// DOM
 let body = document.querySelector('body')
 let hamburgerIcon = document.querySelector('.mobile-menu-div')
 let mobileMenuItems = document.querySelector('.mobile-menu-items')
 
- // Mobile menu
- hamburgerIcon.addEventListener('click', () => {
-   body.classList.toggle('stop-scroll')
-   mobileMenuItems.classList.toggle('open')
+// Mobile menu
+hamburgerIcon.addEventListener('click', () => {
+    body.classList.toggle('stop-scroll')
+    mobileMenuItems.classList.toggle('open')
    
-   for(let i = 0; i < 3; i++){
-       hamburgerIcon.children[i].classList.toggle('open')
-   }
+    for (let i = 0; i < 3; i++) {
+        hamburgerIcon.children[i].classList.toggle('open')
+    }
 })
-mobileMenuItems.addEventListener('click', () => {
-   body.classList.toggle('stop-scroll')
-   mobileMenuItems.classList.toggle('open')
 
-   for(let i = 0; i < 3; i++){
-       hamburgerIcon.children[i].classList.toggle('open')
-   }
+mobileMenuItems.addEventListener('click', () => {
+    body.classList.toggle('stop-scroll')
+    mobileMenuItems.classList.toggle('open')
+
+    for (let i = 0; i < 3; i++) {
+        hamburgerIcon.children[i].classList.toggle('open')
+    }
 })
